b386cbd73b58b18c5a51e6e89359ed32615cbd97
jerricoo <jerriconatanael19@gmail.com>
2025-06-04 11:23:21 +0700

    Menambahkan OrderController dan merevisi AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,4 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Cart;
 use App\Models\User;
---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Order;
