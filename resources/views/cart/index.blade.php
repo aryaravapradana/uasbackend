9ad7683483c3f21f2b0621d87d489236581bc6bd
Perancesco <francescoalexander1234@gmail.com>
2025-06-09 17:17:55 +0700

    fitur + - bekerja, redirect, total harga

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
     <meta charset="UTF-8">
-    <title>Keranjang Saya</title>
+    <title>Keranjang Anda</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://cdn.tailwindcss.com"></script>
