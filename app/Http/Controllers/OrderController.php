96f6f887fab9498831ff1ac28fd98278a29b6eac
Perancesco <francescoalexander1234@gmail.com>
2025-06-09 20:59:44 +0700

    controller untuk ordercontroller.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,5 +1,21 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
+
 use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller
+{
+    public function show(Order $order)
+    {
+        if ($order->user_id !== Auth::id()) {
+            abort(403);
+        }
+
+        $order->load(['items.product', 'payment']);
+
+        return view('order.show', compact('order'));
+    }
+}
